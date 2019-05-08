a8903663bdb3ed38cd15c34345e705d0c4ac5487
Dominik Nowicki <dominik.nowicki@biokod.pl>
2019-05-08 18:55:53 +0200

    compass feature

--- app/src/main/java/pl/donow/compass/MainActivity.kt
+++ app/src/main/java/pl/donow/compass/MainActivity.kt
@@ -1,12 +1,62 @@
 package pl.donow.compass
 
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var currentOrientation = 0f
+    private var orientationMatrix = FloatArray(3)
+    private var rotationMatrix = FloatArray(9)
+    private val sensorManager by lazy {
+        getSystemService(SENSOR_SERVICE) as SensorManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        sensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+            val orientation = SensorManager.getOrientation(rotationMatrix, orientationMatrix)[0]
+            rotatePointerView(Math.toDegrees(orientation.toDouble()).toFloat())
+        }
+    }
+
+    private fun rotatePointerView(orientation: Float) {
+        val animation = RotateAnimation(
+            currentOrientation,
+            -orientation,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        animation.duration = 210
+        animation.fillAfter = true
+        pointerImageView.startAnimation(animation)
+        currentOrientation = -orientation
     }
 }
